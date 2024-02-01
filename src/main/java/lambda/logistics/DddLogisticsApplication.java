61d3bb8e4ed4663ae029da6d1b8478be3b6a12a5
ningth <ningth@inspur.com>
2024-02-01 16:59:54 +0800

    feat: init

---
+++ src/main/java/lambda/logistics/DddLogisticsApplication.java
@@ -0,0 +1,13 @@
+package lambda.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddLogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddLogisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lambda/logistics/DddLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package lambda.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddLogisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
